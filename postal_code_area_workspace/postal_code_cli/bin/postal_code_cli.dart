207b857cb648ba70c0add3eccab8797d55d35757
2024100103t-stack <2024100103t@psu.palawan.edu.ph>
2026-09-13 19:27:28 +0800

    Delete postal_code_area_workspace directory

--- postal_code_area_workspace/postal_code_cli/bin/postal_code_cli.dart
+++
@@ -1,5 +0,0 @@
-import 'package:postal_code_cli/postal_code_cli.dart' as postal_code_cli;
-
-void main(List<String> arguments) {
-  print('Hello world: ${postal_code_cli.calculate()}!');
-}
--- postal_code_area_workspace/terminal_colors/lib/terminal_colors.dart
+++
@@ -1,1 +0,0 @@
-export 'src/ansi.dart' show TerminalColor, Colorizer;
